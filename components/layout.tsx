9648461fa88d570dd366189304a52dd3366a8b26
Abby <sk573129@gmail.com>
2022-09-22 00:20:58 +0800

    feat: 1.layout樣板開發 2.使用google fonts

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import styles from './layout.module.scss';
 
 type LayoutProps = {
     children: JSX.Element;
@@ -11,8 +12,11 @@ export default function Layout({ children }: LayoutProps) {
             <Head>
                 <title>English Treasure</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                {/* Google fonts */}
+                <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet" />
             </Head>
-            <main>{children}</main>
+            <header className={styles.header}>English Treasure</header>
+            <main className={styles.main}>{children}</main>
         </>
     )
 };
