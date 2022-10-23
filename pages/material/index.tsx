9333fe1b8ebe5f69c5a4dbaa6a54077039661d53
Abby <sk573129@gmail.com>
2022-10-23 23:39:11 +0800

    feat: 語料內頁樣板-根據業務邏輯調整UI，不要單字和句子在同一層

--- pages/material/index.tsx
+++ pages/material/index.tsx
@@ -21,30 +21,37 @@ const MaterialPage: NextPage = () => {
     const dataList = [
         {
             id: 'md00001',
-            content: [
-                { id: 'w00001', type: '1', english: 'trying', phoneticSymbol: '/ˈtraɪɪŋ/', chinese: [{ type: 'v', mean: '嘗試；審問；審理；磨練；折磨；測試' }, { type: 'adj', mean: '折磨；使苦惱' }] }, // 1.單字
-            ]
+            type: '1', // 1.單字
+            english: 'trying',
+            phoneticSymbol: '/ˈtraɪɪŋ/',
+            chineseList: [{ type: 'v', mean: '嘗試；審問；審理；磨練；折磨；測試' }, { type: 'adj', mean: '折磨；使苦惱' }]
         },
         {
             id: 'md00002',
-            content: [
-                { id: 'w00002', type: '1', english: 'perspective', phoneticSymbol: '/pɚˈspɛktɪv/', chinese: [{ type: 'n', mean: '透視；觀點；態度' }] }, // 1.單字
-            ]
+            type: '1',// 1.單字
+            english: 'perspective',
+            phoneticSymbol: '/pɚˈspɛktɪv/',
+            chineseList: [{ type: 'n', mean: '透視；觀點；態度' }]
         },
         {
             id: 'md00003',
-            content: [
-                { id: 'w00003', type: '1', english: 'uncertainty', phoneticSymbol: '/ʌnˈsɝ.tən.ti/', chinese: [{ type: 'n', mean: '不確定性' }] }, // 1.單字
-                { id: 'w00004', type: '3', english: 'Life is full of uncertainties.', chinese: '人生充滿了未知數' }, //3.句子
-                { id: 'w00005', type: '3', english: 'Nothing is ever decided, and all the uncertainty is very bad for staff morale.', chinese: '甚麽都還沒有決定，而這一切不確定因素都對員工士氣非常不利。' }, //3.句子
-            ]
+            type: '1',// 1.單字
+            english: 'uncertainty',
+            phoneticSymbol: '/ʌnˈsɝ.tən.ti/',
+            chineseList: [{ type: 'n', mean: '不確定性' }]
         },
         {
             id: 'md00004',
-            content: [
-                { id: 'w00006', type: '2', english: 'well-balanced diet', chinese: '飲食均衡' }, //2.片語
-            ]
-        }
+            type: '3',//3.句子
+            english: 'Life is full of uncertainties.',
+            chinese: '人生充滿了未知數'
+        },
+        {
+            id: 'md00005',
+            type: '2',//2.片語
+            english: 'well-balanced diet',
+            chinese: '飲食均衡'
+        },
     ];
 
     return (
@@ -63,38 +70,33 @@ const MaterialPage: NextPage = () => {
             <div className={styles['material-content']}>
                 {dataList.map((item) => (
                     <div key={item.id} className={styles['material-item']}>
-                        {item.content.map((v) => {
-                            return (
-                                <div key={v.id}>
-                                    {v.type === '1'
-                                        ? (
-                                            <div className={styles['material-word']}>
-                                                <div className={styles['material-english']}>{v.english}</div>
-                                                <div className={styles['description']}>
-                                                    {/* 音標 */}
-                                                    <div>{v.phoneticSymbol}</div>
-                                                    {/* 中文 */}
-                                                    {v.chinese.map((c) => {
-                                                        return (
-                                                            <div key={c.type}>
-                                                                <span>{`(${c.type}.) `}</span>
-                                                                <span className={styles['material-chinese']}>{c.mean}</span>
-                                                            </div>
-                                                        )
-                                                    })}
+                        {item.type === '1'
+                            ? (
+                                <div className={styles['material-word']}>
+                                    <div className={styles['material-english']}>{item.english}</div>
+                                    <div className={styles['description']}>
+                                        {/* 音標 */}
+                                        <div>{item.phoneticSymbol}</div>
+                                        {/* 中文 */}
+                                        {item.chineseList?.map((c) => {
+                                            return (
+                                                <div key={c.type}>
+                                                    <span>{`(${c.type}.) `}</span>
+                                                    <span className={styles['material-chinese']}>{c.mean}</span>
                                                 </div>
-                                            </div>
-                                        )
-                                        : (
-                                            <div>
-                                                <div className={styles['material-english']}>{v.english}</div>
-                                                <div className={styles['material-chinese-sentence']}>{v.chinese}</div>
-                                            </div>
-                                        )
-                                    }
+                                            )
+                                        })}
+                                    </div>
+
                                 </div>
                             )
-                        })}
+                            : (
+                                <div>
+                                    <div className={styles['material-english']}>{item.english}</div>
+                                    <div className={styles['material-chinese-sentence']}>{item.chinese}</div>
+                                </div>
+                            )
+                        }
                     </div>
                 ))}
             </div>
